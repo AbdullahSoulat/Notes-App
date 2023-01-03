0cc0a65b9091a6a8978de632b0b73228d3dbfee6
AbdullahSoulat <abdullahsoulat40@gmail.com>
2023-01-03 19:18:33 +0500

    added local storage support

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const addNoteBtn = document.querySelector('.add-note');
 const saveNoteBtn = document.querySelector('.save-btn');
 
+const titleTag = document.querySelector('input');
+const descriptionTag = document.querySelector('textarea');
+
+const months = ['January', 'February', 'March','April', 'May', 'June', 'July', 'August',
+                'September', 'October', 'November', 'December'];
+// getting the localStorage notes if they exist and parsing them to a javascript object or else passing an empty array to notes
+const notes = JSON.parse(localStorage.getItem('notes') || '[]');
 const menu = document.getElementById('app');
 noteEditor = document.querySelector(".popup-box");
 
@@ -18,7 +25,16 @@ addNoteBtn.addEventListener('click', () => {
     menu.style.zIndex = '-100';
 })
 
-saveNoteBtn.addEventListener('click', () => {
+function showNotes() {
+    notes.forEach((note) => {
+        let newNote = ``
+    })
+}
+showNotes();
+
+saveNoteBtn.addEventListener('click', e => {
+    e.preventDefault();
+
     // removing the note editing window
     noteEditor.classList.remove('show');
     noteEditor.classList.add('hidden');
@@ -30,4 +46,75 @@ saveNoteBtn.addEventListener('click', () => {
     // making the menu window accessible
     menu.style.zIndex = '100';
     noteEditor.style.zIndex = '-100';
+
+    let noteTitle = titleTag.value;
+    let noteDescription = descriptionTag.value;
+    if (noteTitle || noteDescription) {
+        newDate = formatDate();
+
+        let noteInfo = {
+            title: noteTitle,
+            description: noteDescription,
+            date: `${newDate.month} ${newDate.day}, ${newDate.year}`
+        }
+
+        // storing the note info the browser's local storage
+        notes.push(noteInfo); 
+        localStorage.setItem('notes', JSON.stringify(notes)); 
+    }
 })
+
+
+function formatDate() {
+    // getting the year, month and day from the current date
+    let dateObject = new Date();
+    let year = dateObject.getFullYear();
+    let month = dateObject.getMonth();
+    let day = dateObject.getDate();
+
+    // getting the month as text and not a number
+    switch (month) {
+        case 0:
+            newMonth = months[0];
+            break;
+        case 1:
+            newMonth = months[1];
+            break;
+        case 2:
+            newMonth = months[2];
+            break;
+        case 3:
+            newMonth = months[3];
+            break;
+        case 4:
+            newMonth = months[4];
+            break;
+        case 5:
+            newMonth = months[5];
+            break;
+        case 6:
+            newMonth = months[6];
+            break;
+        case 7:
+            newMonth = months[7];
+            break;
+        case 8:
+            newMonth = months[8];
+            break;
+        case 9:
+            newMonth = months[9];
+            break;
+        case 10:
+            newMonth = months[10];
+            break;
+        case 11:
+            newMonth = months[11];
+            break;
+    };
+
+    return newDateObject = {
+        month: newMonth,
+        day: day,
+        year: year
+    }
+}
